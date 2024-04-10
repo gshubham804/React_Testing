33733e74d85aa10111ab0181ff0749545fd378c2
SHUBHAM GUPTA <gshubham804@gmail.com>
2024-04-10 10:04:01 +0530

    update the threshold

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
-export default {
-    testEnvironment: "jsdom",
-    moduleNameMapper: {
-      "^.+\\.svg$": "jest-svg-transformer",
-      "^.+\\.(css|less|scss)$": "identity-obj-proxy"
+module.exports = {
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^.+\\.svg$": "jest-svg-transformer",
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy"
+  },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
     }
-  };
-  
+  }
+};
